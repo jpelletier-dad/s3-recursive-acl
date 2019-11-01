67b0688cd2a44e3d2d4938187e6c7460c303cd48
Jake Pelletier <jake@digitalassetsdata.com>
2019-11-01 10:31:31 -0600

    add support for StartAfter and MaxKeys

--- s3-recursive-acl.go
+++ s3-recursive-acl.go
@@ -12,23 +12,31 @@ import (
 )
 
 func main() {
-	var bucket, region, path, cannedACL string
+	var bucket, region, path, cannedACL, startAfter string
+	var maxKeys int64
+
 	var wg sync.WaitGroup
 	var counter int64
+
 	flag.StringVar(&region, "region", "ap-northeast-1", "AWS region")
 	flag.StringVar(&bucket, "bucket", "s3-bucket", "Bucket name")
 	flag.StringVar(&path, "path", "/", "Path to recurse under")
 	flag.StringVar(&cannedACL, "acl", "public-read", "Canned ACL to assign objects")
+	flag.Int64Var(&maxKeys, "max-keys", 1000, "Maximum keys per page")
+	flag.StringVar(&startAfter, "start-after", "", "Key to start after")
 	flag.Parse()
 
 	svc := s3.New(session.New(), &aws.Config{
 		Region: aws.String(region),
 	})
 
-	err := svc.ListObjectsPages(&s3.ListObjectsInput{
-		Prefix: aws.String(path),
-		Bucket: aws.String(bucket),
-	}, func(page *s3.ListObjectsOutput, lastPage bool) bool {
+	err := svc.ListObjectsV2Pages(&s3.ListObjectsV2Input{
+		Prefix:     aws.String(path),
+		Bucket:     aws.String(bucket),
+		MaxKeys:    &maxKeys,
+		StartAfter: aws.String(startAfter),
+	}, func(page *s3.ListObjectsV2Output, lastPage bool) bool {
+		fmt.Println(fmt.Sprintf("Done '%d'", counter))
 		for _, object := range page.Contents {
 			key := *object.Key
 			counter++
@@ -46,6 +54,7 @@ func main() {
 				defer wg.Done()
 			}(bucket, key, cannedACL)
 		}
+		//wg.Wait()
 		return true
 	})
 
